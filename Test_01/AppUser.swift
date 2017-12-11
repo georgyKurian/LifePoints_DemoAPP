7508caa6dcb2e83237ae0082953e4f9aa7215a90
Georgi Kurian <georgy.v.k@gmail.com>
2017-12-10 20:42:39 -0500

    Showing user list

---
+++ Test_01/AppUser.swift
@@ -0,0 +1,15 @@
+//
+//  AppUser.swift
+//  Test_01
+//
+//  Created by Jithin Paul on 2017-12-10.
+//  Copyright © 2017 Jithin Paul. All rights reserved.
+//
+
+import UIKit
+
+class AppUser:NSObject{
+    var username:String?
+    var bio:String?
+    var gps:String?
+}
--- Test_01/UsersTableViewControlle.swift
+++ Test_01/UsersTableViewControlle.swift
@@ -7,89 +7,53 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabaseUI
 
 class UsersTableViewControlle: UITableViewController {
 
+    var userList = [AppUser]();
+    var dbRef:DatabaseReference?
+    var dbHandler:DatabaseHandle?
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        dbRef = Database.database().reference().child("/users")
+        self.getData()
     }
-
+    
+    func getData(){
+        dbHandler = dbRef?.observe(DataEventType.childAdded, with: { (snapshot) in
+            print("------ New user to the list")
+            let dataChanged = snapshot.value as! [String:String]
+            print(dataChanged)
+            
+            let newUser = AppUser()
+            newUser.username = dataChanged["username"]!
+            newUser.bio = dataChanged["bio"]!
+            newUser.gps = dataChanged["gps"]!
+            
+            self.userList.append(newUser)
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        })
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return self.userList.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "userRow", for: indexPath)
+        cell.textLabel!.text = userList[indexPath.row].username
+        cell.detailTextLabel!.text = userList[indexPath.row].bio
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
